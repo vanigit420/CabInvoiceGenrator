10509c8eec6761eaf7c042ef6f2790ae7437023a
shivani korwan <shivanikorwan63232@gmail.com>
2022-07-02 10:24:47 +0530

    UC1 calculating total fare for single unit

---
+++ CabInvoiceGenrator/CabInvoiceGenrator/CabInvoice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenrator
+{
+    public class CabInvoice
+    {
+        const int COST_PER_KM = 10;
+        const int COST_PER_MINUTE = 1;
+        const int MINIMUM_FARE = 5;
+        public int CalculateFare(int distance, int time)
+        {
+            int fare = (distance * COST_PER_KM) + (time * COST_PER_MINUTE);
+
+            if (fare > MINIMUM_FARE)
+            {
+                return fare;
+            }
+            return MINIMUM_FARE;
+
+        }
+    }
+}
---
+++ CabInvoiceGenrator/CabInvoiceGenrator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CabInvoiceGenerator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the CabInvoiceGenerator problem");
+        }
+    }
+}
